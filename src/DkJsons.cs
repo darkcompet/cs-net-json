932483a649898a5dede90fa4592790765523e588
DarkCompet <darkcompet@gmail.com>
2025-06-14 22:12:39 +0700

    Add option ignore

--- src/DkJsons.cs
+++ src/DkJsons.cs
@@ -1,6 +1,7 @@
 namespace Tool.Compet.Json;
 
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 /// Json helper for converter Json vs Object.
 /// See https://www.jacksondunstan.com/articles/3303 to choose best approach on each serialize/deserialize.
@@ -9,18 +10,39 @@ using System.Text.Json;
 /// - Deserialize small string => Unity is optimal
 /// - Deserialize large string => Newton is optimal
 public class DkJsons {
-	private static readonly JsonSerializerOptions WriteIndentedOption = new() { WriteIndented = true };
-	private static readonly JsonSerializerOptions NotWriteIndentedOption = new() { WriteIndented = false };
+	private static readonly JsonSerializerOptions Option_Indent = new() {
+		WriteIndented = true,
+	};
+	private static readonly JsonSerializerOptions Option_Indent_Ignore = new() {
+		WriteIndented = true,
+		DefaultIgnoreCondition = JsonIgnoreCondition.Never
+	};
+	private static readonly JsonSerializerOptions Option_NoIndent = new() {
+		WriteIndented = false
+	};
+	private static readonly JsonSerializerOptions Option_NoIndent_Ignore = new() {
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.Never
+	};
 
 	/// <summary>
 	/// Convert obj to json string.
 	/// Each field/properties in the object should be annotated with [JsonPropertyName()] attribute
 	/// </summary>
 	/// <param name="obj"></param>
-	/// <param name="writeIndented">Default false to compact size (minification optimized)</param>
+	/// <param name="indent">Also write indent or not. Default is false to compact size (minification optimized)</param>
+	/// <param name="ignore">Skip Serialize/Deserialize ignored fields or not. Default is true to skip ignored fields.</param>
 	/// <returns></returns>
-	public static string ToJson(object obj, bool writeIndented = false) {
-		return JsonSerializer.Serialize(obj, options: writeIndented ? WriteIndentedOption : NotWriteIndentedOption);
+	public static string ToJson(object obj, bool indent = false, bool ignore = true) {
+		var options = indent ?
+			(ignore ? Option_Indent_Ignore : Option_Indent) :
+			(ignore ? Option_NoIndent_Ignore : Option_NoIndent);
+
+		return JsonSerializer.Serialize(obj, options: options);
+	}
+
+	public static string ToJson(object obj, JsonSerializerOptions options) {
+		return JsonSerializer.Serialize(obj, options: options);
 	}
 
 	/// <summary>
@@ -28,10 +50,19 @@ public class DkJsons {
 	/// Each field/properties in the object should be annotated with [JsonPropertyName()] attribute
 	/// </summary>
 	/// <param name="obj"></param>
-	/// <param name="writeIndented">Default false to compact size (minification optimized)</param>
+	/// <param name="indent">Also write indent or not. Default is false to compact size (minification optimized)</param>
+	/// <param name="ignore">Skip Serialize/Deserialize ignored fields or not. Default is true to skip ignored fields.</param>
 	/// <returns></returns>
-	public static string? ToJsonOrNull(object? obj, bool writeIndented = false) {
-		return obj is null ? null : JsonSerializer.Serialize(obj, options: writeIndented ? WriteIndentedOption : NotWriteIndentedOption);
+	public static string? ToJsonOrNull(object? obj, bool indent = false, bool ignore = true) {
+		var options = indent ?
+			(ignore ? Option_Indent_Ignore : Option_Indent) :
+			(ignore ? Option_NoIndent_Ignore : Option_NoIndent);
+
+		return obj is null ? null : JsonSerializer.Serialize(obj, options: options);
+	}
+
+	public static string? ToJsonOrNull(object? obj, JsonSerializerOptions options) {
+		return obj is null ? null : JsonSerializer.Serialize(obj, options: options);
 	}
 
 	/// <summary>
@@ -46,6 +77,10 @@ public class DkJsons {
 		return json is null ? null : JsonSerializer.Deserialize<T>(json);
 	}
 
+	public static T? ToObjWith<T>(string? json, JsonSerializerOptions options) where T : class {
+		return json is null ? null : JsonSerializer.Deserialize<T>(json, options: options);
+	}
+
 	/// <summary>
 	/// Convert given JsonElement to given type without try/catch.
 	/// JsonElement is created when dotnet parse payload to object.
@@ -56,6 +91,13 @@ public class DkJsons {
 	public static T? ToObjFromJsonElement<T>(object element) where T : class {
 		if (element is JsonElement jsonElement) {
 			return jsonElement.Deserialize<T>();
+		}
+		return null;
+	}
+
+	public static T? ToObjFromJsonElement<T>(object element, JsonSerializerOptions options) where T : class {
+		if (element is JsonElement jsonElement) {
+			return jsonElement.Deserialize<T>(options: options);
 		}
 		return null;
 	}
